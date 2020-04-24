d2c2685aa1b929e0d239038a37768e8412d79cae
jerryhopper <hopper.jerry@gmail.com>
2020-04-24 22:54:53 +0200

    ssl detect

--- src/Swoole/SlimSwoole.php
+++ src/Swoole/SlimSwoole.php
@@ -5,24 +5,32 @@ use Swoole\Coroutine\Server\Connection;
 
 use Pachico\SlimSwoole\BridgeManager;
 
+$_SSL = false;
+if( file_exists("/etc/osbox/ssl/ssl.cert") ){
 
 
-$certpath = "/etc/osbox/ssl/blackbox.surfwijzer.nl/ssl.cert";
+    $certpath = "/etc/osbox/ssl/blackbox.surfwijzer.nl/ssl.cert";
+    $certpath = "/etc/osbox/ssl/ssl.cert";
 
-$certinfo = openssl_x509_parse(file_get_contents($certpath));
+    $certinfo = openssl_x509_parse(file_get_contents($certpath));
 
-$valid_from = date(DATE_RFC2822,$certinfo['validFrom_time_t']);
-$valid_to = date(DATE_RFC2822,$certinfo['validTo_time_t']);
+    $valid_from = date(DATE_RFC2822,$certinfo['validFrom_time_t']);
+    $valid_to = date(DATE_RFC2822,$certinfo['validTo_time_t']);
 
-if( $certinfo['validFrom_time_t'] > time() || $certinfo['validTo_time_t'] < time() ){
-    //print "Certificate is expired.";
-    error_log("Certificate is expired.  ".$valid_to);
-}else{
-    //error_log("valid to ".$certinfo['validTo_time_t']);
-    error_log("SSLCert Valid From: ".$valid_from);
-    error_log("SSLCert Valid To:".$valid_to);
+    if( $certinfo['validFrom_time_t'] > time() || $certinfo['validTo_time_t'] < time() ){
+        //print "Certificate is expired.";
+        error_log("Certificate is expired.  ".$valid_to);
+    }else{
+        //error_log("valid to ".$certinfo['validTo_time_t']);
+        error_log("SSLCert Valid From: ".$valid_from);
+        error_log("SSLCert Valid To:".$valid_to);
+
+    }
+    $_SSL =true;
+
 
 }
+
 
 ##################################################################################################################
 
@@ -37,16 +45,8 @@ $bridgeManager = new BridgeManager($app);
 #$xhttp = new Swoole\HTTP\Server("0.0.0.0");
 #$port1 = $server->listen("127.0.0.1", 9501, SWOOLE_SOCK_TCP);
 
-$srv = new Swoole\HTTP\Server("0.0.0.0", 9501,SWOOLE_BASE, SWOOLE_SOCK_TCP);
-//$srv = new Swoole\HTTP\Server("0.0.0.0", 9501, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
 
-
-// setup the location of ssl cert files and key files
-$ssl_dir="/etc/osbox/ssl/blackbox.surfwijzer.nl";
-
-$srv->set([
-    'ssl_cert_file' => $ssl_dir . '/ssl.cert',
-    'ssl_key_file' => $ssl_dir . '/ssl.key',
+$options = array(
     'pid_file' => '/var/run/osbox.pid',
     'daemonize' => true,
     'worker_num'=> 2,
@@ -55,7 +55,23 @@ $srv->set([
     'log_level' => 0,
     'log_file' => '/var/log/swoole.log',
     //'open_http2_protocol' => true, // Enable HTTP2 protocol
-]);
+);
+
+
+
+if(!$_SSL){
+    $srv = new Swoole\HTTP\Server("0.0.0.0", 9501,SWOOLE_BASE, SWOOLE_SOCK_TCP);
+}else{
+    $srv = new Swoole\HTTP\Server("0.0.0.0", 9501, SWOOLE_PROCESS, SWOOLE_SOCK_TCP | SWOOLE_SSL);
+    $ssl_dir="/etc/osbox/ssl";
+
+    $options['ssl_cert_file'] = $ssl_dir . '/ssl.cert';
+    $options['ssl_key_file']  = $ssl_dir . '/ssl.key';
+
+
+}
+
+$srv->set($options);
 
 
 
