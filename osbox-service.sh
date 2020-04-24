0542b3af19240375b89ccdde496f0d7243e7c824
jerryhopper <hopper.jerry@gmail.com>
2020-04-24 18:11:04 +0200

    fixes

--- osbox-service.sh
+++ osbox-service.sh
@@ -88,7 +88,7 @@ echo $PROG
 
 
 
-
+echo "*********************************"
 cd /usr/local/osbox
 
 PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d /usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini -f /usr/local/osbox/project/sw-osbox-core/src/osbox-service.php
