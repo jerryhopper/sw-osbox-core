5592ccbbe5f82c666e1e8360eef0a65ba826a82b
jerryhopper <hopper.jerry@gmail.com>
2020-04-24 12:35:18 +0200

    added env for composer

--- osbox-update.sh
+++ osbox-update.sh
@@ -214,8 +214,8 @@ done
 cd ..
 
 
-PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d
-/usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini /usr/local/osbox/bin/composer.phar --working-dir=/usr/local/osbox/project/sw-osbox-core update
+#PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d
+PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d /usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini /usr/local/osbox/bin/composer.phar --working-dir=/usr/local/osbox/project/sw-osbox-core update
 
 
 
