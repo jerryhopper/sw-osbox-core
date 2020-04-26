b1afa95f631d961754a05dbcbc9681be304ca8ab
jerryhopper <hopper.jerry@gmail.com>
2020-04-26 16:21:32 +0200

    osbox cmdline options

--- osbox.sh
+++ osbox.sh
@@ -3,11 +3,18 @@
 
 if [ "$1" == "update" ]; then
     /usr/local/osbox/osbox-update
+    exit;
 fi
 
 
 if [ "$1" =="watch" ]; then
   echo "Watching codebase for changes."
   PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d nohup /usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini -f /usr/local/osbox/project/sw-osbox-core/src/autoreload-daemon.php & /dev/null
-
+  exit;
 fi
+
+
+
+echo "Usage: "
+echo "osbox update  - updates the codebase."
+echo "osbox watch  - watches the codebase for changes and reloads swoole."
