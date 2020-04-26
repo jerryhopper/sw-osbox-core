a53781e78b31dc6e1cf7c334a6d3c0c874657d16
jerryhopper <hopper.jerry@gmail.com>
2020-04-26 16:11:23 +0200

    inotify

---
+++ src/autoreload-daemon.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__.'/Swoole/ToolKit/AutoReload.php';
+
+$filename = "/var/run/osbox.pid";
+$handle = fopen($filename, "r");
+$contents = fread($handle, filesize($filename));
+fclose($handle);
+
+
+
+$kit = new Swoole\ToolKit\AutoReload((int)trim($contents));
+$kit->watch(__DIR__.'');
+$kit->run();
