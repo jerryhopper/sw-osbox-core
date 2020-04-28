289acc1af947f5032e775ea443a2a00c6a4f52a7
jerryhopper <hopper.jerry@gmail.com>
2020-04-28 12:55:23 +0200

    removed php mdns

--- osbox.sh
+++ osbox.sh
@@ -19,6 +19,11 @@ if [ "$1" == "watch" ]; then
 fi
 
 #
+if [ "$1" == "test" ]; then
+  echo "Watching codebase for changes."
+  PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d nohup /usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini -f /usr/local/osbox/project/sw-osbox-core/src/test.php
+  exit;
+fi
 
 
 echo "Usage: "
