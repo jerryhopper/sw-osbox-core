df8c1714988aef5481d3e27a3a05de428d767f68
jerryhopper <hopper.jerry@gmail.com>
2020-05-01 00:22:21 +0200

    added cmdline options

--- osbox.sh
+++ osbox.sh
@@ -11,6 +11,12 @@ if [ "$1" == "reinstall" ]; then
     exit;
 fi
 
+if [ "$1" == "reboot" ]; then
+    nohup sudo reboot & >/dev/null
+    exit;
+fi
+
+
 
 if [ "$1" == "watch" ]; then
   echo "Watching codebase for changes."
@@ -26,6 +32,67 @@ if [ "$1" == "test" ]; then
 fi
 
 
+# osbox network functions
+if [ "$1" == "scheduler" ]; then
+  if [ "$2" == "restart" ]; then
+    systemctl restart osbox-scheduler
+  fi
+  if [ "$2" == "start" ]; then
+    systemctl start osbox-scheduler
+  fi
+  if [ "$2" == "stop" ]; then
+    systemctl stop osbox-scheduler
+  fi
+
+  # command information
+  if [ "$2" == "" ]; then
+    echo "Usage: "
+    echo "  osbox scheduler restart"
+    echo "  osbox scheduler start"
+    echo "  osbox scheduler stop"
+
+
+  fi
+fi
+
+
+
+
+# osbox network functions
+if [ "$1" == "network" ]; then
+
+  # if - interfaces
+  if [ "$2" == "if" ]; then
+
+      # osbox if reset
+      if [ "$3" == "reset" ]; then
+          echo "network reset"
+      fi
+      # osbox if set <ip>
+      if [ "$3" == "set" ]; then
+          echo "network set x y z"
+      fi
+
+  fi
+
+
+  # command information
+  if [ "$2" == "" ]; then
+    echo "Usage: "
+    echo "  osbox network if reset  - Resets the network to dhcp"
+    echo "  osbox network if set <IP> - Sets the network to static ip."
+
+  fi
+  #echo "Watching codebase for changes."
+  #PHP_INI_SCAN_DIR=/usr/local/osbox/bin/conf.d nohup /usr/local/osbox/bin/osboxd -c /usr/local/osbox/bin/osboxd.ini -f /usr/local/osbox/project/sw-osbox-core/src/test.php
+  exit;
+fi
+
+
+
 echo "Usage: "
-echo "osbox update  - updates the codebase."
-echo "osbox watch  - watches the codebase for changes and reloads swoole."
+echo "  osbox network  - show network functions."
+echo "  osbox update  - updates the codebase."
+echo "  osbox watch  - watches the codebase for changes and reloads swoole."
+echo "  osbox reboot  - reboots the device."
+echo "  osbox test  - runs a dev test script"
