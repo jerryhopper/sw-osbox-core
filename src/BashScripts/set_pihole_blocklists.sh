949c45ee75b60b3b141c547a7c3163341c86f3a0
jerryhopper <hopper.jerry@gmail.com>
2020-05-29 23:05:28 +0200

    updated blocklist urls

--- src/BashScripts/set_pihole_blocklists.sh
+++ src/BashScripts/set_pihole_blocklists.sh
@@ -2,13 +2,8 @@
 
 
 
-    echo "https://blocklists.surfwijzer.nl/c/porn">/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/c/proxy">>/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/c/ads">>/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/c/tracking">>/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/c/shorturl">>/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/g/1">>/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/g/2">>/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/g/3">>/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/g/4">>/etc/pihole/adlists.list
-    echo "https://blocklists.surfwijzer.nl/g/5">>/etc/pihole/adlists.list
+    echo "https://setup.surfwijzer.nl/blacklist/porn">/etc/pihole/adlists.list
+    echo "https://setup.surfwijzer.nl/blacklist/proxy">>/etc/pihole/adlists.list
+    echo "https://setup.surfwijzer.nl/blacklist/ads">>/etc/pihole/adlists.list
+    echo "https://setup.surfwijzer.nl/blacklist/tracking">>/etc/pihole/adlists.list
+    echo "https://setup.surfwijzer.nl/blacklist/shorturl">>/etc/pihole/adlists.list
