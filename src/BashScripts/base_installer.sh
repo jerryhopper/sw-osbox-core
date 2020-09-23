b4fd6cc9286ff61d906ab3e4bbaf7efdc14f0a73
jerryhopper <hopper.jerry@gmail.com>
2020-09-23 16:37:41 +0200

    test install pihole

--- src/BashScripts/base_installer.sh
+++ src/BashScripts/base_installer.sh
@@ -8,8 +8,17 @@ log(){
 }
 
 
+# is_command function
+is_command() {
+    # Checks for existence of string passed in as only function argument.
+    # Exit value of 0 when exists, 1 if not exists. Value is the result
+    # of the `command` shell built-in call.
+    local check_command="$1"
+    command -v "${check_command}" >/dev/null 2>&1
+}
 
-function createUser{
+
+createUser(){
     # adduser
     log "Adding $OSBOX_BIN_USR user."
     if id -u osbox >/dev/null 2>&1; then
