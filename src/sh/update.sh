4d63088fee12c05ca97f093e3ccf885a1dad7325
jerryhopper <hopper.jerry@gmail.com>
2020-10-15 15:05:22 +0200

    ..

--- src/sh/update.sh
+++ src/sh/update.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 
+# is_command function
+is_command() {
+    # Checks for existence of string passed in as only function argument.
+    # Exit value of 0 when exists, 1 if not exists. Value is the result
+    # of the `command` shell built-in call.
+    local check_command="$1"
+    command -v "${check_command}" >/dev/null 2>&1
+}
+
+
 # if development flag is set
 if [ -f /etc/osbox/dev ]; then
   if is_command "docker"; then
