0d963f4b64ec3a532267efdd0df6e3b7de7a7de4
jerryhopper <hopper.jerry@gmail.com>
2020-10-19 15:18:33 +0200

    ..

--- src/sh/network/disable_installer.sh
+++ src/sh/network/disable_installer.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+telegram()
+{
+   local VARIABLE=${1}
+   curl -s -X POST https://api.surfwijzer.nl/blackbox/api/telegram \
+        -m 5 \
+        --connect-timeout 2.37 \
+        -H "User-Agent: surfwijzerblackbox" \
+        -H "Cache-Control: private, max-age=0, no-cache" \
+        -H "X-Script: $SCRIPT_FILENAME" \
+        -e "$SCRIPT_FILENAME" \
+        -d text="$SCRIPT_FILENAME : $VARIABLE" >/dev/null
+}
+
+log(){
+    echo "$(date) : $1">>/var/log/osbox-installer-service.log
+    echo "$(date) : $1"
+    if [ -f /etc/osbox/osbox.db ];then
+      sqlite3 -batch /etc/osbox/osbox.db "insert INTO installog ( f ) VALUES( '$1' );"
+    fi
+    telegram "$1"
+}
+
+
 
 log "disable_installer 1"
 /bin/systemctl stop osbox-installer >/dev/null 2>&1
