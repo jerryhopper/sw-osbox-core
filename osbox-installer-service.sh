85641375b89b8b00a35a4007afb2cb53bb527e18
jerryhopper <hopper.jerry@gmail.com>
2020-10-20 21:04:28 +0200

    ..

--- osbox-installer-service.sh
+++ osbox-installer-service.sh
@@ -101,7 +101,7 @@ osbox_listener_service(){
     echo "">>/etc/systemd/system/osbox-listener.service
     echo "[Service]">>/etc/systemd/system/osbox-listener.service
     echo "Type=simple">>/etc/systemd/system/osbox-listener.service
-    echo "PIDFile=/run/osbox-listener">>/etc/systemd/system/osbox-listener.service
+    #echo "PIDFile=/run/osbox-listener">>/etc/systemd/system/osbox-listener.service
     echo "Restart=always">>/etc/systemd/system/osbox-listener.service
     echo "RestartSec=30">>/etc/systemd/system/osbox-listener.service
     echo "User=root">>/etc/systemd/system/osbox-listener.service
