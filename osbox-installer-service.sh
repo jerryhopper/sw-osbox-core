d3a88a56db49f2dbf06e40ceed5122952a709b42
jerryhopper <hopper.jerry@gmail.com>
2020-10-22 16:28:52 +0200

    ..

--- osbox-installer-service.sh
+++ osbox-installer-service.sh
@@ -97,26 +97,32 @@ docker_rm(){
 ## type forking
 ## simple
 osbox_listener_service(){
-    echo "[Unit]">/etc/systemd/system/osbox-listener.service
-    echo "Description=osbox-listener-service">>/etc/systemd/system/osbox-listener.service
-    echo "After=network.target">>/etc/systemd/system/osbox-listener.service
+
+    if [ -f /etc/systemd/system/osbox-listener.service ] ; then
+        rm -f /etc/systemd/system/osbox-listener.service
+    fi
+    ln -s /usr/local/osbox/lib/systemd/osbox-listener-service /etc/systemd/system/osbox-listener.service
+
+    #echo "[Unit]">/etc/systemd/system/osbox-listener.service
+    #echo "Description=osbox-listener-service">>/etc/systemd/system/osbox-listener.service
+    #echo "After=network.target">>/etc/systemd/system/osbox-listener.service
     #echo "StartLimitIntervalSec=4">>/etc/systemd/system/osbox-listener.service
-    echo "">>/etc/systemd/system/osbox-listener.service
-    echo "[Service]">>/etc/systemd/system/osbox-listener.service
-    echo "Type=simple">>/etc/systemd/system/osbox-listener.service
-    echo "PIDFile=/run/osbox-listener">>/etc/systemd/system/osbox-listener.service
-    echo "Restart=on-abort">>/etc/systemd/system/osbox-listener.service
+    #echo "">>/etc/systemd/system/osbox-listener.service
+    #echo "[Service]">>/etc/systemd/system/osbox-listener.service
+    #echo "Type=simple">>/etc/systemd/system/osbox-listener.service
+    #echo "PIDFile=/run/osbox-listener">>/etc/systemd/system/osbox-listener.service
+    #echo "Restart=on-abort">>/etc/systemd/system/osbox-listener.service
     #echo "RestartSec=30">>/etc/systemd/system/osbox-listener.service
-    echo "User=root">>/etc/systemd/system/osbox-listener.service
-    echo "TimeoutStartSec=5">>/etc/systemd/system/osbox-listener.service
-    echo "TimeoutStopSec=5">>/etc/systemd/system/osbox-listener.service
+    #echo "User=root">>/etc/systemd/system/osbox-listener.service
+    #echo "TimeoutStartSec=5">>/etc/systemd/system/osbox-listener.service
+    #echo "TimeoutStopSec=5">>/etc/systemd/system/osbox-listener.service
 
-    echo "ExecStart=/usr/local/osbox/bin/listen.sh">>/etc/systemd/system/osbox-listener.service
-    echo "KillSignal=SIGKILL">>/etc/systemd/system/osbox-listener.service
-    #echo "TasksMax=10">>/etc/systemd/system/osbox-listener.service
-    echo "[Install]">>/etc/systemd/system/osbox-listener.service
-    echo "WantedBy=multi-user.target">>/etc/systemd/system/osbox-listener.service
-
+    #echo "ExecStart=/usr/local/osbox/bin/listen.sh">>/etc/systemd/system/osbox-listener.service
+    #echo "KillSignal=SIGKILL">>/etc/systemd/system/osbox-listener.service
+    ##echo "TasksMax=10">>/etc/systemd/system/osbox-listener.service
+    #echo "[Install]">>/etc/systemd/system/osbox-listener.service
+    #echo "WantedBy=multi-user.target">>/etc/systemd/system/osbox-listener.service
+    systemctl daemon-reload
     systemctl enable osbox-listener
     systemctl start osbox-listener
 }
@@ -126,6 +132,7 @@ osbox_updater_service(){
         rm -f /etc/systemd/system/osbox-updater.service
     fi
     ln -s /usr/local/osbox/lib/systemd/osbox-updater-service /etc/systemd/system/osbox-updater.service
+    systemctl daemon-reload
     systemctl enable osbox-updater
     systemctl start osbox-updater
 }
--- osbox-updater-service.sh
+++ osbox-updater-service.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-sleep 50
+sleep 5
+
+
+# Disable the installer service.
+INSTALLER="$(systemctl is-active osbox-installer.service)"
+INSTALLER_SERVICE="$(systemctl is-enabled osbox-installer.service)"
+
+if "$INSTALLER" = "active" ; then
+    systemctl stop osbox-installer.service
+fi
+if "$INSTALLER_SERVICE" = "enabled" ; then
+    systemctl disable osbox-installer.service
+fi
+
+# ...
+
+sleep 10
