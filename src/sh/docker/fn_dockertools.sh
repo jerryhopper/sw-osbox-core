6156697649b0866a3b02724320fcafdd81377660
jerryhopper <hopper.jerry@gmail.com>
2020-11-21 00:17:28 +0100

    dockertools

--- src/sh/docker/fn_dockertools.sh
+++ src/sh/docker/fn_dockertools.sh
@@ -6,26 +6,44 @@ source /usr/local/osbox/bin/fn/DownloadUnpack.fn
 source /usr/local/osbox/bin/fn/GetRemoteVersion.fn
 source /usr/local/osbox/bin/fn/IsOnline.fn
 
+
 runDockerApp(){
     REPO_ORG="$1"
     REPO_REPO="$2"
-
     INSTALL_PATH="/opt/osbox/$REPO_REPO"
+
     if [ ! -d $INSTALL_PATH ];then
         echo "App does not exist"
         exit 1
     fi
     docker-compose -f "$INSTALL_PATH/docker-compose.yml" up -d
 }
+
 getDockerApp(){
     REPO_ORG="$1"
     REPO_REPO="$2"
-
     INSTALL_PATH="/opt/osbox/$REPO_REPO"
+
     if [ -d $INSTALL_PATH ];then
         echo "App already exists"
         exit 1
     fi
-
     DownloadUnpack "$REPO_ORG" "$REPO_REPO" "$(GetRemoteVersion "$REPO_ORG" "$REPO_REPO")" "$INSTALL_PATH"
+}
+
+stopDockerApp(){
+    REPO_ORG="$1"
+    REPO_REPO="$2"
+    INSTALL_PATH="/opt/osbox/$REPO_REPO"
+
+    docker-compose -f "$INSTALL_PATH/docker-compose.yml" stop
+}
+
+removeDockerApp(){
+    REPO_ORG="$1"
+    REPO_REPO="$2"
+    INSTALL_PATH="/opt/osbox/$REPO_REPO"
+
+    docker-compose -f "$INSTALL_PATH/docker-compose.yml" down
+    rm -rf $INSTALL_PATH
 }
