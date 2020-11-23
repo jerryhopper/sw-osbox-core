a4958553afe119fb715fd61c1d2f72e0513d3a1a
jerryhopper <hopper.jerry@gmail.com>
2020-11-23 17:03:40 +0100

    get/run DockerApp

---
+++ src/sh/runDockerApp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+#
+USAGE = echo "Usage: runDockerApp.sh "GITHUBORG" "GITHUBREPO""
+#
+
+if [ "$1" == "" ];then
+  echo "Error! Missing Organization \n$USAGE";
+  exit 1
+fi
+
+if [ "$2" == "" ];then
+  echo "Error! Missing Repository \n$USAGE";
+  exit 1
+fi
+
+source /usr/local/osbox/project/sw-osbox-core/src/sh/docker/fn_dockertools.sh
+
+#getDockerApp "$1" "$2"
+runDockerApp "$1" "$2"
+#stopDockerApp "jerryhopper" "sw-osbox-pihole"
+#removeDockerApp "jerryhopper" "sw-osbox-pihole"
+
+exit
