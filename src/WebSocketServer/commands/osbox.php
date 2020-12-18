6575c8ac0ccfab76213efc999d68da2aaacb05ee
jerryhopper <hopper.jerry@gmail.com>
2020-12-18 18:31:42 +0100

    ..

--- src/WebSocketServer/commands/osbox.php
+++ src/WebSocketServer/commands/osbox.php
@@ -99,6 +99,43 @@ class osbox extends CommandBase {
         });
     }
 
+    function authpoll($args=""){
+        $pusher = $this->pusher; // Required!
+
+
+        $command = "osbox auth poll"; // The issued command
+
+
+        go(function () use ($command, $pusher) {
+            /*
+             * Here you can do tasks and push info to the websocket
+             * There are 3 message variants.
+             *
+             *  $pusher->push( "RESULT","command", array() )
+             *  $pusher->push( "INFO", "title", "text" )
+             *  $pusher->push( "ERROR", "errormessage", "$e->getmessage()" )
+             *
+             */
+
+            # Execute the command, and get the results.
+            $ret = Co\System::exec($command);
+
+            $ret['code'];
+            $ret['signal'];
+            $ret['output'];
+            echo "output:";
+            #var_dump($ret['output']);
+            //$ret['output'] = explode("\n", $ret['output']);
+            var_dump($ret['output']);
+
+
+            $pusher->push("RESULT", $command, $ret);
+
+
+        });
+    }
+
+
     function auth($args=""){
         $pusher = $this->pusher; // Required!
 
@@ -124,8 +161,8 @@ class osbox extends CommandBase {
             $ret['signal'];
             $ret['output'];
             echo "output:";
-            var_dump($ret['output']);
-            $ret['output'] = explode("\n", $ret['output']);
+           #var_dump($ret['output']);
+            //$ret['output'] = explode("\n", $ret['output']);
             var_dump($ret['output']);
 
 
